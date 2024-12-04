9f163f3d9ed1628b16d34cc62b491f3ed613cf4f
hippo <jackyhung0211@gmail.com>
2024-12-04 16:33:47 +0800

    上傳20241204上課資料

---
+++ 00.VScode/JavaScript/20241204-content/20241204/vue-project/src/components/BottomFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="footer">網頁設計及維護：10洪士杰、最近更新日期：2024/10/18</div>
+</template>
+
+<style scoped lang="sass">
+#footer
+  width: 1000px
+  height: 20px
+  margin: auto
+  clear: both
+  font-size: 16px
+  font-family: '標楷體'
+  color: #000
+  text-align: center
+</style>
---
+++ 00.VScode/JavaScript/20241204-content/20241204/vuetify-project/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <VApp>
+    <VAppBar>
+      <VContainer class="d-flex align-center">
+        <VAppBarTitle>VUETIFY</VAppBarTitle>
+        <v-btn prepend-icon="mdi-home" to="/">首頁</v-btn>
+        <v-btn prepend-icon="mdi-cog" to="/test">測試頁</v-btn>
+      </VContainer>
+    </VAppBar>
+    <VMain>
+      <RouterView></RouterView>
+    </VMain>
+  </VApp>
+</template>
